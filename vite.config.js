50b032183e8131112d5ce1796d924130dc793441
nira0101 <nira0101@stud.kea.dk>
2021-12-02 14:34:00 +0100

    navigation done

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path')
+
+module.exports = {
+    base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+       
+      }
+    }
+  }
+}
