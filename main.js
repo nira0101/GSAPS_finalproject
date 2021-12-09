7ee4e3e79f02c4340634769f8f81205f2757a10b
nira0101 <nira0101@stud.kea.dk>
2021-12-09 14:06:26 +0100

    form validation and restdb database done

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
 import './sass/style.scss'
 
-"use strict"
-window.addEventListener("DOMContentLoaded", init);
 
+window.addEventListener("DOMContentLoaded", init);
+const formOne = document.querySelector("#form-c");
+
+
+function init(){
+  post();
+/*   validateForm(); */
+ 
+  }
+  
+  function post(data){
+    const postData = JSON.stringify(data);
+    fetch('https://gspscontact-a453.restdb.io/rest/contact-form', {
+      method: "post",
+      headers:  { 'cache-control': 'no-cache',
+      'x-apikey': '61b0b1da72a03f5dae8221e9',
+      'content-type': 'application/json;  charset=utf-8' },
+      body: postData,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
+
+  //validate form//
+  function validateForm(){
+    console.log(validateForm)
+    
+    if(!formOne.elements.name.checkValidity()){
+      document.querySelector("#error-2").classList.add("hidden");
+      document.querySelector("#error-1").classList.remove("hidden");
+     }
+     else if(!formOne.elements.email.checkValidity()){
+      document.querySelector("#error-1").classList.add("hidden");
+      document.querySelector("#error-2").classList.remove("hidden");
+     }
+     else{
+      document.querySelector("#error-1").classList.add("hidden");
+      document.querySelector("#error-2").classList.add("hidden");
+    }
+  }
+
+  
+
+    document.getElementById("submit").addEventListener("click", (e) => {
+e.preventDefault();
+
+validateForm();
+post({
+  name: formOne.elements.name.value,
+  email: formOne.elements.email.value,
+ number: formOne.elements.number.value,
+ desc:formOne.elements.desc.value
+});
+
+    })
+
+    
+
+//mobile nav//
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
 
@@ -29,3 +87,11 @@ window.addEventListener("resize", () => {
     }, 1000);
   }
 });
+
+  
+
+ 
+ 
+
+  formOne.setAttribute("novalidate", true);
+
